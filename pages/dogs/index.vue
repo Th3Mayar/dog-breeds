71f456d241ae833fa5cbb4107375fe1d4ef06103
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-06-13 15:01:54 -0400

    chore: add animation to title dog breeds on dogs

--- pages/dogs/index.vue
+++ pages/dogs/index.vue
@@ -56,4 +56,23 @@ export default defineComponent({
     flex-direction: column;
   }
 }
+h1{
+  font-weight: 800;
+  background-image: url('https://images.aeonmedia.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/essay-final-gettyimages-685469924.jpg?width=3840&quality=75&format=auto');
+  background-size: 400% 400% ;
+  animation: gradient 10s infinite;
+  color: transparent;
+  background-clip: text;
+}
+@keyframes gradient{
+  0%{
+    background-position: 0% 50%;
+  }
+  50%{
+    background-position: 100% 50%;
+  }
+  100%{
+    background-position: 0% 50%;
+  }
+}
 </style>
